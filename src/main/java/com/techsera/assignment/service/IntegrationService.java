967d9fe6a15041a35158319691fa55837b7772e7
Kirushan <kirushanbalakrishnan@gmail.com>
2021-11-21 22:09:59 +0530

    Change method names

--- src/main/java/com/techsera/assignment/service/IntegrationService.java
+++ src/main/java/com/techsera/assignment/service/IntegrationService.java
@@ -1,7 +1,6 @@
 package com.techsera.assignment.service;
 
 import com.techsera.assignment.constant.CustomerStatus;
-import com.techsera.assignment.controller.CustomerController;
 import com.techsera.assignment.entity.Customer;
 import com.techsera.assignment.repository.CustomerRepository;
 import org.slf4j.Logger;
@@ -11,23 +10,19 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.transformer.support.StaticHeaderValueMessageProcessor;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 import java.util.Optional;
 
 @Component
 public class IntegrationService {
-    private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
+    private static final Logger log = LoggerFactory.getLogger(IntegrationService.class);
+
     @Autowired
     CustomerRepository customerRepository;
 
@@ -39,7 +34,7 @@ public class IntegrationService {
     }
 
     @ServiceActivator(inputChannel = "router.channel", outputChannel ="router.channel.getObject")
-    public Message<Customer> anotherMessage(Message<Customer> message) throws MessagingException{
+    public Message<Customer> receiveMessage(Message<Customer> message) throws MessagingException{
         Optional<Customer> customer=customerRepository.findByNameAndPassword(message.getPayload().getName(),message.getPayload().getPassword());
         if(customer.isPresent()){
             if(customer.get().getCustomerStatus()== CustomerStatus.ACTIVE){
@@ -53,8 +48,7 @@ public class IntegrationService {
 
 
     @ServiceActivator(inputChannel = "router.objectToJson.channel")
-    public void anotherMessage2(Message<Customer> message) {
-
+    public void sendCall(Message<Customer> message) {
 
         RestTemplate restTemplate=new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange("https://jsonplaceholder.typicode.com/posts", HttpMethod.POST, new HttpEntity(message.getPayload(), new HttpHeaders()),String.class);
@@ -62,7 +56,7 @@ public class IntegrationService {
     }
 
     @ServiceActivator(inputChannel = "router.channel.logInfo")
-    public void anotherMessage3(Message<Customer> message) {
+    public void logFailed(Message<Customer> message) {
 
         log.info(message.getPayload().toString());
     }
